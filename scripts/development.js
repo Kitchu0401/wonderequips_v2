bfcd27c53ed50a2c25926ce8c6306d265c8458ff
Kitchu0401 <kitchu@naver.com>
2017-04-16 14:58:23 +0900

    daily commit: fixed build and start scripts for production environment

--- scripts/development.js
+++ scripts/development.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'development';
 
 var nodemon = require('nodemon');
-nodemon('--exec babel-node --presets=es2015 ./src/main.js --watch ./src');
+nodemon('--exec babel-node --presets=es2015 ./server/main.js --watch ./server');
 
 nodemon.on('start', function() {
     console.log('[nodemon] App has started');
--- server/main.js
+++ server/main.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import session from 'express-session';
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
-import mongoose from 'mongoose';
+import express              from 'express';
+import session              from 'express-session';
+import webpack              from 'webpack';
+import WebpackDevServer     from 'webpack-dev-server';
+import mongoose             from 'mongoose';
 
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
+import morgan               from 'morgan';
+import bodyParser           from 'body-parser';
 
-import path from 'path';
-import route from './routes/route';
+import path                 from 'path';
+import route                from './routes/route';
 
-const app = express();
-const port = 3000;
-const devPort = 4000;
+const app                   = express();
+const port                  = 3000;
+const devPort               = 4000;
 
 // Setting up middleware: body-parser
 app.use(morgan('dev'));
@@ -28,19 +28,19 @@ app.use(session({
 
 // Serving static files
 // app.use(express.static('./../public'));
-app.use('/', express.static(path.join(__dirname, './../public')));
-
+app.use('/', express.static(path.join(__dirname, './../build')));
 app.use('/api', route);
 
 const db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', () => { console.log('Connected to mongodb server.') });
+      db.on('error', console.error);
+      db.once('open', () => { console.log('Connected to mongodb server.') });
 mongoose.connect('mongodb://localhost/wonderequips');
 
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
 
+// Start devServer if served env value.
 if ( process.env.NODE_ENV === 'development' ) {
     console.log('Server is running on development mode');
     // const config = require('../webpack.dev.config');
@@ -53,13 +53,3 @@ if ( process.env.NODE_ENV === 'development' ) {
         }
     );
 }
-
-
-// package.json scripts backup
-// "scripts": {
-//     "build": "babel src -d build --presets=es2015",
-//     "start": "node ./build/main.js",
-//     "development": "NODE_ENV=development nodemon --exec babel-node --presets=es2015 ./src/main.js --watch server",
-//     "win_development": "set NODE_ENV=development&nodemon --exec babel-node --presets=es2015 ./src/main.js --watch server",
-//     "test": "echo \"Error: no test specified\" && exit 1"
-// },
