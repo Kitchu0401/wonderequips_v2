df9cd6ee63770eb432a30046fe290fb745385e07
Kitchu0401 <kitchu@naver.com>
2017-05-01 23:29:51 +0900

    adjust temporal baseUrl to service functions

--- src/services/service.js
+++ src/services/service.js
@@ -2,20 +2,22 @@
 
 import axios from 'axios';
 
+const baseUrl = '/WonderEquips/api';
+
 // Configuring axios instance
-const instance = axios.create({
-    baseUrl: '/WonderEquips/api',
-    timeout: 2500
-});
+// const instance = axios.create({
+//     baseUrl: '/WonderEquips/api',
+//     timeout: 2500
+// });
 
 export function insertLog() {
-    return instance.post('/log', { type: 'Search' });
+    return instance.post(`${baseUrl}/log`, { type: 'Search' });
 }
 
 export function getMessageList() {
-    return instance.get('/message');
+    return instance.get(`${baseUrl}/message`);
 }
 
 export function insertMessage(content) {
-    return instance.post('/message', { content: content });
+    return instance.post(`${baseUrl}/message`, { content: content });
 }
